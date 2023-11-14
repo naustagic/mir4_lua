a403c34e68c6006f3d3e30d415133d70bd3a084b
“âzhenghao1819 <963417379@qq.com>
2023-11-14 14:20:39 +0800

    测试mail_ent

--- mir4/Script/game/entities/mail_ent.lua
+++ mir4/Script/game/entities/mail_ent.lua
@@ -44,11 +44,11 @@ function mail_ent.get_mail()
     for i = 1, 5 do
         local num = mail_unit.get_mail_num_byidx(i)
         if num > 0 then
-            -- 阅读并领取 12
+            -- 阅读并领取 10
             -- git测试
             mail_unit.read_mail(i)
             decider.sleep(2000)
-            -- 删除 12
+            -- 删除 10
             mail_unit.del_mail(i)
             decider.sleep(2000)
         end
